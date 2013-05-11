21f1865d97e977225a986ffcab366c84bfaecca9
Hack Reactor Students <github@hackreactor.com>
2013-05-11 16:54:44 -0700

    clicking song in queue plays song.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ var App = Backbone.Model.extend({
     this.set('currentSong', new Song());
     this.set('songQueue', new SongQueue());
 
-    // params.library.on('enqueue', function(song){
-    //   this.set('currentSong', song);
-    // }, this);
+    this.get('songQueue').on('play', function(song){
+      this.set('currentSong', song);
+    }, this);
 
     params.library.on('enqueue', function(song) {
       this.get('songQueue').add(song);
