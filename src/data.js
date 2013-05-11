d986137bb52697cec2a21a927e8e37365da6c815
Hack Reactor Students <github@hackreactor.com>
2013-05-11 12:31:17 -0700

    Adding all mp3s to data

--- src/data.js
+++ src/data.js
@@ -18,5 +18,50 @@ var songData = [
     url: "mp3s/04 One In A Million.mp3",
     title: "One In A Million",
     artist: "Aaliyah"
+  },
+  {
+    title: "It's Whatever",
+    artist: "Aaliyah",
+    url: "mp3s/It's Whatever.mp3"
+  },
+
+  {
+    title: "Age Ain't Nothing But A Number",
+    artist: "Aaliyah",
+    url: "mp3s/Age Ain't Nothing But A Number.mp3"
+  },
+
+
+  {
+    title: "If Your Girl Only Knew",
+    artist: "Aaliyah",
+    url: "mp3s/If Your Girl Only Knew.mp3"
+  },
+  {
+    title: "Choosey Lover [Old School_New School]",
+    artist: "Aaliyah",
+    url: "mp3s/Choosey Lover [Old School_New School].mp3"
+  },
+
+  {
+    title: "Everything's Gonna Be Alright",
+    artist: "Aaliyah",
+    url: "mp3s/Everything's Gonna Be Alright.mp3"
+  },
+
+  {
+    title: "Rock The Boat",
+    artist: "Aaliyah",
+    url: "mp3s/Rock The Boat.mp3"
+  },
+  {
+    title: "More Than A Woman",
+    artist: "Aaliyah",
+    url: "mp3s/More Than A Woman.mp3"
+  },
+  {
+    title: "Read Between The Lines",
+    artist: "Aaliyah",
+    url: "mp3s/Read Between The Lines.mp3"
   }
 ];
