a403d6251d93a7a3637255623bf69e21110f72c5
Charles Holbrow <charlesholbrow@gmail.com>
2013-05-11 21:48:43 -0700

    fix song data filenames

--- src/data.js
+++ src/data.js
@@ -27,36 +27,36 @@ var songData = [
   {
     title: "Age Ain't Nothing But A Number",
     artist: "Aaliyah",
-    url: "mp3s/Age Ain't Nothing But A Number.mp3"
+    url: "mp3s/03 Age Ain't Nothing But A Number.mp3"
   },
   {
     title: "If Your Girl Only Knew",
     artist: "Aaliyah",
-    url: "mp3s/If Your Girl Only Knew.mp3"
+    url: "mp3s/06 If Your Girl Only Knew.mp3"
   },
   {
     title: "Choosey Lover [Old School_New School]",
     artist: "Aaliyah",
-    url: "mp3s/Choosey Lover [Old School_New School].mp3"
+    url: "mp3s/07 Choosey Lover [Old School_New School].mp3"
   },
   {
     title: "Everything's Gonna Be Alright",
     artist: "Aaliyah",
-    url: "mp3s/Everything's Gonna Be Alright.mp3"
+    url: "mp3s/09 Everything's Gonna Be Alright.mp3"
   },
   {
     title: "Rock The Boat",
     artist: "Aaliyah",
-    url: "mp3s/Rock The Boat.mp3"
+    url: "mp3s/12 Rock The Boat.mp3"
   },
   {
     title: "More Than A Woman",
     artist: "Aaliyah",
-    url: "mp3s/More Than A Woman.mp3"
+    url: "mp3s/14 More Than A Woman.mp3"
   },
   {
     title: "Read Between The Lines",
     artist: "Aaliyah",
-    url: "mp3s/Read Between The Lines.mp3"
+    url: "mp3s/15 Read Between The Lines.mp3"
   }
 ];
